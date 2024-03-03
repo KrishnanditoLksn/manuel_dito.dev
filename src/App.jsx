46dc8630241eda90806286644ec76f5412dd8817
Gorthdzx <krislaksana123@gmail.com>
2024-03-03 11:02:09 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import {useState} from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Menu from "./components/navbar/Menu.jsx";
+
+export default function App() {
+    return (
+        <>
+            <div>
+                <Menu></Menu>
+            </div>
+        </>
+    )
+}
