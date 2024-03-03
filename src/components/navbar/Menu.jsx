bc70154f324b5b229665a697e87b3798a90fe96e
Gorthdzx <krislaksana123@gmail.com>
2024-03-03 11:21:11 +0700

    refactor : navbar structure and style

--- src/components/navbar/Menu.jsx
+++ src/components/navbar/Menu.jsx
@@ -1,13 +1,13 @@
 const header = () => {
     return <header>
-        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-            <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <nav className="flex items-center justify-between flex-wrap bg-white p-6">
+            <div className="flex items-center flex-shrink-0 text-blue-950 hover:text-blue-700 mr-6">
                 <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
                      xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
                 </svg>
-                <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+                <span className="font-semibold text-xl tracking-tight">Ditogakngoding</span>
             </div>
             <div className="block lg:hidden">
                 <button
@@ -18,15 +18,18 @@ const header = () => {
                     </svg>
                 </button>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto justify-around">
+            <div className="w-full  flex lg:flex  lg:w-auto justify-between">
                 <div className="text-sm">
-                    <a href="" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    <a href="#"
+                       className="block mt-4 lg:inline-block lg:mt-0 text-blue-900 font-bold hover:text-blue-700 mr-4">
                         Docs
                     </a>
-                    <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-blue-100 mr-4">
+                    <a href="#"
+                       className="block mt-4 lg:inline-block lg:mt-0 text-blue-900 font-bold hover:text-blue-700 mr-4">
                         Examples
                     </a>
-                    <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                    <a href="#"
+                       className="block mt-4 lg:inline-block lg:mt-0 text-blue-900 font-bold hover:text-blue-700 mr-4">
                         Blog
                     </a>
                 </div>
