3b8e2a892ccef7cb266a17963ac05a578bb244c5
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-24 21:29:58 +0700

    feat :  short description content section

---
+++ src/components/section/ContentSection.jsx
@@ -0,0 +1,11 @@
+import DescriptionContent from "./DescriptionContent"
+
+const ContentSection = () => {
+    return (
+        <section id="coder">
+                <DescriptionContent></DescriptionContent>
+        </section>
+    )
+}
+
+export default ContentSection
