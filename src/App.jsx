264e9d0fa01c038d9b706a0a7f54c2bb07cced57
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-24 20:31:17 +0700

    refac : components structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import Navbar from "./components/Navbar.jsx";
 import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
 import ContactMe from "./components/ContactMe.jsx";
+import Header from "./components/Navbar.jsx";
 
 function App() {
     return (
         <main className="text-gray-400 bg-gray-900 body-font">
-            <Navbar></Navbar>
+            <Header></Header>
             <About></About>
             <Skills></Skills>
             <ContactMe></ContactMe>
