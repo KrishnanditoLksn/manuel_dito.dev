a5367219af35bf93ab4442d0571e9eb34a9c1d8c
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-25 05:40:57 +0700

    feat : added content section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Navbar from "./components/Navbar.jsx";
 import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
 import ContactMe from "./components/ContactMe.jsx";
-import Header from "./components/Navbar.jsx";
+import Header from "./components/pages/navbar/Navbar.jsx";
+import ContentSection from "./components/section/ContentSection.jsx";
 
 function App() {
     return (
         <main className="text-gray-400 bg-gray-900 body-font">
             <Header></Header>
             <About></About>
+            <ContentSection></ContentSection>
             <Skills></Skills>
             <ContactMe></ContactMe>
         </main>
