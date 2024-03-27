3b4238f166428082e59d2dbfc832f4911978e283
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-27 10:33:43 +0700

    feat : about button

---
+++ src/components/pages/about/AboutButton.jsx
@@ -0,0 +1,17 @@
+const AboutButton = () => {
+    return (
+        <div dir="ltr">
+            <div className="me-30">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-40 sm:mt-9 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300">
+                    Get Started
+                </button>
+                <button
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-40 sm:mt-9 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300">
+                    More Information
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default AboutButton
