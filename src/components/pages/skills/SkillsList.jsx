4a5d03ac3ed3207e6f264e5bc0e19eb8ce3a5704
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-27 12:03:12 +0700

    feat : grid layout template

---
+++ src/components/pages/skills/SkillsList.jsx
@@ -0,0 +1,21 @@
+const SkillsList = () => {
+    return (
+        <div className=" ml-46 place-items-center  grid grid-cols-1 gap-2">
+            <h1 className="text-2xl font-bold">My SKills</h1>
+            <div>
+                <img className="w-60 h-60 rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+                </img>
+            </div>
+            <div>
+                <img className="w-60 h-60 rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+                </img>
+            </div>
+            <div>
+                <img className="w-60 h-60 rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+                </img>
+            </div>
+        </div>
+    )
+}
+
+export default SkillsList
