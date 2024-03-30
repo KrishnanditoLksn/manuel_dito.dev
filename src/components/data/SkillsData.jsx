1b2f6c75b67226ba3344f21663f43d6198a71302
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-30 13:13:30 +0700

    refac : changed to array of objects

---
+++ src/components/data/SkillsData.jsx
@@ -0,0 +1,22 @@
+const SkillsData = [
+    {
+        id: 1,
+        name: "Java",
+        Est: 2022,
+        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+    },
+    {
+        id: 2,
+        name: "Kotlin",
+        Est: 2023,
+        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+    },
+    {
+        id: 3,
+        name: "Python",
+        Est: 2022,
+        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+    }
+]
+
+export default SkillsData
