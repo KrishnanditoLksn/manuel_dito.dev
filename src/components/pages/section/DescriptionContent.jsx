6f280e500b41f9ff677fc015cfc6cf5f1d55aba8
KrishnanditoLksn <krislaksana123@gmail.com>
2024-03-30 13:28:09 +0700

    refac : react className

--- src/components/pages/section/DescriptionContent.jsx
+++ src/components/pages/section/DescriptionContent.jsx
@@ -1,17 +1,17 @@
 const DescriptionContent = () => {
     return (
         <section className="bg-white dark:bg-gray-900">
-            <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-72">
+            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-72">
                 <div>
-                    <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400 mb-80">
-                        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Dito from Sragen</h2>
-                        <p class="mb-4">Non Wibu boy; Yoasobi fans since 2022 </p>
-                        <p class="mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia sapiente temporibus, recusandae nobis quo deserunt velit nihil unde iste, officia voluptatibus hic voluptate! Consequatur pariatur mollitia asperiores ipsam sint maiores, delectus iure odit vero, amet doloremque veritatis itaque quidem ullam earum dolor aspernatur fuga esse error sit voluptates, expedita voluptatibus quisquam. Quos, perspiciatis unde vitae excepturi quia iure ad corporis qui! Repellat itaque fugit reiciendis accusamus sunt assumenda adipisci, asperiores et, suscipit quod cum quasi ab voluptatem placeat! Magnam, dicta totam modi voluptate doloremque temporibus possimus perferendis inventore ex delectus!
+                    <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400 mb-80">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Dito from Sragen</h2>
+                        <p className="mb-4">Non Wibu boy; Yoasobi fans since 2022 </p>
+                        <p className="mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia sapiente temporibus, recusandae nobis quo deserunt velit nihil unde iste, officia voluptatibus hic voluptate! Consequatur pariatur mollitia asperiores ipsam sint maiores, delectus iure odit vero, amet doloremque veritatis itaque quidem ullam earum dolor aspernatur fuga esse error sit voluptates, expedita voluptatibus quisquam. Quos, perspiciatis unde vitae excepturi quia iure ad corporis qui! Repellat itaque fugit reiciendis accusamus sunt assumenda adipisci, asperiores et, suscipit quod cum quasi ab voluptatem placeat! Magnam, dicta totam modi voluptate doloremque temporibus possimus perferendis inventore ex delectus!
                             Repellat eligendi placeat ipsa deserunt illo explicabo optio quis quo.</p>
                     </div>
                 </div>
                 <div className="space-x-40 ml-72">
-                    <div class="rtl:grid grid-cols-2 gap-4 mt-8 space-x-10">
+                    <div className="rtl:grid grid-cols-2 gap-4 mt-8 space-x-10">
                         <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1"></img>
                         <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2"></img>
                     </div>
