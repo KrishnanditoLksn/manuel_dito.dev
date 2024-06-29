d24dbd3eb07d5c97dc907629cdfda2f22f75c2cc
Gorthdzx <krislaksana123@gmail.com>
2024-06-29 19:12:21 +0700

    refactor : skills src

--- src/components/data/SkillsData.jsx
+++ src/components/data/SkillsData.jsx
@@ -1,22 +1,26 @@
-const SkillsData = [
+import javalogo from '../../assets/img_java.png'
+import kotlinlogo from '../../assets/img_kotlin.png'
+import pythonlogo from '../../assets/img_py.png'
+
+const skills_list    = [
     {
         id: 1,
         name: "Java",
         Est: 2022,
-        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+        src:  javalogo
     },
     {
         id: 2,
         name: "Kotlin",
         Est: 2023,
-        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+        src: kotlinlogo
     },
     {
         id: 3,
         name: "Python",
         Est: 2022,
-        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+        src: pythonlogo
     }
 ]
 
-export default SkillsData
+export default skills_list
