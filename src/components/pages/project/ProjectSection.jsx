616e8fa4f9f09d960a306823f640ff9fe40bf721
Gorthdzx <krislaksana123@gmail.com>
2024-12-16 18:01:11 +0700

    feat: added project section (tba)

---
+++ src/components/pages/project/ProjectSection.jsx
@@ -0,0 +1,13 @@
+export default function ProjectSection (){
+    return (
+        <section className="bg-gray-900">
+            <div   className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-42">
+                <div>
+                    <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400 mb-80">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">My Projects</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
