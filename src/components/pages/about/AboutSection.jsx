652966334a7716cae6693ca0717080667b09b083
Gorthdzx <krislaksana123@gmail.com>
2024-12-17 17:41:44 +0700

    style : changed text color to black

--- src/components/pages/about/AboutSection.jsx
+++ src/components/pages/about/AboutSection.jsx
@@ -1,16 +1,16 @@
 const AboutDescription = () => {
     return (
-        <div className=" mx-auto flex px-14 py-20 md:flex-row flex-col items-center">
+        <div className=" mx-auto flex px-14 py-20 md:flex-row flex-col items-center bg-white">
             <div
                 className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white  ">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black  ">
                     Hi,Emmanuel Dito is here
                 </h1>
                 <br className="hidden lg:inline-block"/>
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-white  ">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-black  ">
                     Passion in Android Development
                 </h1>
-                <p className="mb-2  leading-relaxed">
+                <p className="mb-2  leading-relaxed text-black">
                     Join Bangkit Academy 2024 Batch 2 with Android Development as my learning path.Learn about how
                     android apps run and build.For my There is coding is my passion
                 </p>
