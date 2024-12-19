751a0fbfd0c6dbd19df9ba93cd3c6d458743dd30
Gorthdzx <krislaksana123@gmail.com>
2024-12-19 22:12:43 +0700

    feat :  added rendering list to display all of my projects

--- src/components/pages/project/ProjectContent.jsx
+++ src/components/pages/project/ProjectContent.jsx
@@ -1,14 +1,37 @@
-export default function ProjectContent(){
+import projectList from "../../data/ProjectsData.jsx";
+
+export default function ProjectContent() {
     return (
-        <section id="projects" className="bg-gray-900">
+        <section id="projects" className="bg-white">
             <div
                 className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-42">
                 <div>
-                    <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400 mb-80">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">My
-                            Projects</h2>
+                    <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">
+                            My Projects</h2>
                     </div>
                 </div>
+            </div>
+            <div className="grid grid-cols-3 gap-3 px-4 mx-auto max-w-screen-xl mb-80">
+                {projectList().map((project) => (
+                    <div key={project.keyList} className="max-w-sm rounded overflow-hidden shadow-lg">
+                        <a href={project.link} aria-current={true}>
+                            <img className="w-full"
+                                 src={project.image}
+                                 alt={project.subtitle}/>
+                        </a>
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">{project.title}</div>
+                            <p className="text-gray-700 text-base">
+                                {project.description}
+                            </p>
+                        </div>
+                        <div className="px-6 pt-4 pb-2">
+                        <span
+                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{project.category}</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
     )
