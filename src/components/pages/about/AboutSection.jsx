439bbe7272f4ed47901e400c00f0e3864c3813b6
Gorthdzx <krislaksana123@gmail.com>
2024-12-21 13:37:08 +0700

    refactor : changed description text

--- src/components/pages/about/AboutSection.jsx
+++ src/components/pages/about/AboutSection.jsx
@@ -8,11 +8,14 @@ const AboutDescription = () => {
                 </h1>
                 <br className="hidden lg:inline-block"/>
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-black  ">
-                    Passion in Android Development
+                    Mobile Native Enthusiast;
                 </h1>
-                <p className="mb-2  leading-relaxed text-black">
-                    Join Bangkit Academy 2024 Batch 2 with Android Development as my learning path.Learn about how
-                    android apps run and build.For my There is coding is my passion
+                <p className="mb-2  text-2xl leading-relaxed text-black">
+                    {/* eslint-disable-next-line react/no-unescaped-entities */}
+                    Hello everyone,I'm Dito from Universitas Sanata Dharma.Nice to meet you ols.
+                    I always trust that the best way to learn is by doing, so I always try to make a project to improve
+                    my skills.
+                    So far counted from 2024, I have been a Yoasobi fans and I am very interested in Mobile Development.
                 </p>
                 <br className="hidden lg:inline-block"></br>
             </div>
