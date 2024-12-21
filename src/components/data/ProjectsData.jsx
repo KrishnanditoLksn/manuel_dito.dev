2eb0f02f89f30192ecbdaff22cce78a6e7530daa
Gorthdzx <krislaksana123@gmail.com>
2024-12-21 09:42:14 +0700

    feat :  added list of my finished or currently develop

--- src/components/data/ProjectsData.jsx
+++ src/components/data/ProjectsData.jsx
@@ -1,18 +1,40 @@
-const ProjectsData = () => [
+const projectList = () => [
     {
+        keyList: 1,
         title: "Alpaca Discord bot",
-        subtitle: "MERN Stack",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-1.gif",
-        link: "https://github.com/KrishnanditoLksn/Alpaca"
+        subtitle: "Python",
+        description: "A bot that can use to send memes,ping user,and make jokes.Developed within 1 years",
+        image: "./src/assets/img_alpaca.png",
+        link: "https://github.com/KrishnanditoLksn/Alpaca",
+        category: "personal-project"
     },
     {
-        title: "Java GUI Tandur Shop",
-        subtitle: "Java Stack",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        link: "https://github.com/KrishnanditoLksn/Tandur_Shop_GUI"
+        keyList: 2,
+        title: "Ascipleus Cancer Detection App",
+        subtitle: "Kotlin",
+        image: "./src/assets/img_ascipleus.png",
+        description: "Detect cancer disease using Image Classification and displayed Confidence Score",
+        link: "https://github.com/KrishnanditoLksn/AslepsiusCancerDetection",
+        category: "personal-project"
+    },
+    {
+        keyList: 3,
+        title: "Culture Connect Cafe Recommendation",
+        subtitle: "Kotlin",
+        image: "./src/assets/img_culture.png",
+        category: "capstone-project",
+        description: "Capstone Project at Bangkit Academy 2024.Predict user mood and displayed best cafe recommendations with mood based",
+        link: "https://github.com/CultureConnect2024/CultureConnect-MD"
+    },
+    {
+        keyList: 4,
+        title: "Certain Cerita App",
+        subtitle: "Kotlin",
+        image: "./src/assets/img_ceritain.png",
+        category: "personal-project",
+        description: "Story based app where user can read , and upload their Stories.Developed in 1 month.Users must register their account to access this app.",
+        link: "https://github.com/KrishnanditoLksn/CeritainCerita"
     }
 ]
 
-export default ProjectsData
+export default projectList
