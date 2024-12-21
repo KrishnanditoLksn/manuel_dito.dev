dea30b2e507ead7c7d6a40905bcb6c5f59dcf09a
Gorthdzx <krislaksana123@gmail.com>
2024-12-21 14:05:31 +0700

    feat : added margin top and short desc

--- src/components/pages/section/DescriptionContent.jsx
+++ src/components/pages/section/DescriptionContent.jsx
@@ -1,26 +1,25 @@
 const DescriptionContent = () => {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-white">
             <div
-                className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-42">
+                className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mt-72 mr-44">
                 <div>
-                    <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400 mb-80">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Dito
-                            from Sragen</h2>
-                        <p className="mb-4">Non Wibu boy; Yoasobi fans since 2022 </p>
-                        <p className="mb-4">Majoring Informatics at Universitas Sanata Dharma with System Development as
+                    <div className="font-light text-black sm:text-lg dark:text-gray-400 mb-80">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold  light:text-white  dark:text-black">Short
+                            Introduction again :) </h2>
+                        <p className="mb-4 dark:text-black font-normal">Majoring Informatics with System Development as
                             my concentration</p>
+                        <p className="mb-4 dark:text-black font-normal">As Informatics student,I have a curiosity in
+                            cryptography algorithms
+                            and mobile development.hope someday I can mix them together xD </p>
                     </div>
                 </div>
                 <div>
                     <div className="space-x-40 ml-72">
-                        <div className="rtl:grid grid-cols-2 gap-4 mt-8 space-x-10">
-                            <img className="w-full rounded-lg"
-                                 src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
-                                 alt="office content 1"></img>
-                            <img className="mt-4 w-full lg:mt-10 rounded-lg"
-                                 src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png"
-                                 alt="office content 2"></img>
+                        <div className="rtl:grid grid-cols-2 gap-4 mb-72 space-x-10">
+                            <img className="w-120 h-120 rounded-lg"
+                                 src="https://github.com/user-attachments/assets/6ba2e32a-8688-4e2b-be46-a225408358e5"
+                                 alt="dito image"></img>
                         </div>
                     </div>
                 </div>
