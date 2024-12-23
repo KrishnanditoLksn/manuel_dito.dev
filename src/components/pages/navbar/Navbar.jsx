89c813c0751a7e57f13bfa2f834f7f0bbb8e44bd
Gorthdzx <krislaksana123@gmail.com>
2024-12-23 08:56:16 +0700

    feat:changed navbar background

--- src/components/pages/navbar/Navbar.jsx
+++ src/components/pages/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import Navlist from "./Navlist"
 
 const Navbar = () => {
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 relative">
+        <nav className="bg-white border-gray-200  dark:bg-white relative">
             <div
                 className=" rtl: space-x-reverse max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <div className='mr-100'></div>
