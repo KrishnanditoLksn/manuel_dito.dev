19ccdcecbf5bd13962dbd24f25b01c887ff16804
Gorthdzx <krislaksana123@gmail.com>
2024-12-24 13:24:05 +0700

    feat: added swipe down button to short bio

--- src/components/pages/about/AboutSection.jsx
+++ src/components/pages/about/AboutSection.jsx
@@ -1,4 +1,11 @@
 const AboutDescription = () => {
+    function swipeDown() {
+        window.scrollTo({
+            top: 1000,
+            behavior: "smooth"
+        });
+    }
+
     return (
         <div className=" mx-auto flex px-14 py-20 md:flex-row flex-col items-center bg-white">
             <div
@@ -12,12 +19,16 @@ const AboutDescription = () => {
                 </h1>
                 <p className="mb-2  text-2xl leading-relaxed text-black">
                     {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    Hello everyone,I'm Dito from Universitas Sanata Dharma.Nice to meet you ols.
+                    Hello everyone,I'm Dito from Universitas Sanata Dharma.Nice to meet you guys.
                     I always trust that the best way to learn is by doing, so I always try to make a project to improve
                     my skills.
                     So far counted from 2024, I have been a Yoasobi fans and I am very interested in Mobile Development.
                 </p>
                 <br className="hidden lg:inline-block"></br>
+                <button onClick={swipeDown}
+                        className="mt-80 ml-[645px] mr-80 text-center text-black font-bold text-2xl">
+                    Swipe Down Here 👇
+                </button>
             </div>
         </div>
     )
