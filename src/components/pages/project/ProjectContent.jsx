dd204fe2ada627a67103bcbfe0b256f6cd954d22
Gorthdzx <krislaksana123@gmail.com>
2024-12-24 17:36:20 +0700

    feat: responsive design to projects section

--- src/components/pages/project/ProjectContent.jsx
+++ src/components/pages/project/ProjectContent.jsx
@@ -16,13 +16,18 @@ export default function ProjectContent() {
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-3 gap-3 px-4 mx-auto max-w-screen-xl mb-80">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 mx-auto max-w-screen-xl mb-20">
                 {projectList().map((project) => (
-                    <div key={project.keyList} className="max-w-sm rounded overflow-hidden shadow-lg">
+                    <div
+                        key={project.keyList}
+                        className="bg-white max-w-sm rounded overflow-hidden shadow-lg transition-transform transform hover:scale-105"
+                    >
                         <a href={project.link} aria-current={true}>
-                            <img className="w-full"
-                                 src={project.image}
-                                 alt={project.subtitle}/>
+                            <img
+                                className="w-full h-48 object-cover"
+                                src={project.image}
+                                alt={project.subtitle}
+                            />
                         </a>
                         <div className="px-6 py-4">
                             <div className="font-bold text-xl mb-2">{project.title}</div>
@@ -31,12 +36,15 @@ export default function ProjectContent() {
                             </p>
                         </div>
                         <div className="px-6 pt-4 pb-2">
-                        <span
-                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{project.category}</span>
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {project.category}
+                </span>
                         </div>
                     </div>
                 ))}
             </div>
+
         </section>
     )
 }
