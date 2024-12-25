80612befe58805c44051d96112e453ce369bd07d
Gorthdzx <krislaksana123@gmail.com>
2024-12-25 19:11:08 +0700

    refactor: wrap project list with a href link

--- src/components/pages/project/ProjectContent.jsx
+++ src/components/pages/project/ProjectContent.jsx
@@ -28,19 +28,18 @@ export default function ProjectContent() {
                                 src={project.image}
                                 alt={project.subtitle}
                             />
+                            <div className="px-6 py-4">
+                                <div className="font-bold text-xl mb-2">{project.title}</div>
+                                <p className="text-gray-700 text-base">
+                                    {project.description}
+                                </p>
+                            </div>
+                            <div className="px-6 pt-4 pb-2">
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                {project.category}
+                            </span>
+                            </div>
                         </a>
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">{project.title}</div>
-                            <p className="text-gray-700 text-base">
-                                {project.description}
-                            </p>
-                        </div>
-                        <div className="px-6 pt-4 pb-2">
-                <span
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    {project.category}
-                </span>
-                        </div>
                     </div>
                 ))}
             </div>
