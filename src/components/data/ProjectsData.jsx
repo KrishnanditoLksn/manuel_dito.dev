12233c69dba0a4bf3a5b0db70763c56f4089919d
Gorthdzx <krislaksana123@gmail.com>
2024-12-26 12:27:05 +0700

    refactor: moved all  images to links

--- src/components/data/ProjectsData.jsx
+++ src/components/data/ProjectsData.jsx
@@ -4,7 +4,7 @@ const projectList = () => [
         title: "Alpaca Discord bot",
         subtitle: "Python",
         description: "A bot that can use to send memes,ping user,and make jokes.Developed within 1 years",
-        image: "./src/assets/img_alpaca.png",
+        image: "https://github.com/user-attachments/assets/776092ef-5823-476c-ac57-02d35919949e",
         link: "https://github.com/KrishnanditoLksn/Alpaca",
         category: "personal-project"
     },
@@ -12,7 +12,7 @@ const projectList = () => [
         keyList: 2,
         title: "Ascipleus Cancer Detection App",
         subtitle: "Kotlin",
-        image: "./src/assets/img_ascipleus.png",
+        image: "https://github.com/user-attachments/assets/ec011999-e204-4d49-918e-5d1985188977",
         description: "Detect cancer disease using Image Classification and displayed Confidence Score",
         link: "https://github.com/KrishnanditoLksn/AslepsiusCancerDetection",
         category: "personal-project"
@@ -21,7 +21,7 @@ const projectList = () => [
         keyList: 3,
         title: "Culture Connect Cafe Recommendation",
         subtitle: "Kotlin",
-        image: "./src/assets/img_culture.png",
+        image: "https://github.com/user-attachments/assets/629215fa-9621-4cd9-8fc8-bb26dd90b99f",
         category: "capstone-project",
         description: "Capstone Project at Bangkit Academy 2024.Predict user mood and displayed best cafe recommendations with mood based",
         link: "https://github.com/CultureConnect2024/CultureConnect-MD"
@@ -30,7 +30,7 @@ const projectList = () => [
         keyList: 4,
         title: "Certain Cerita App",
         subtitle: "Kotlin",
-        image: "./src/assets/img_ceritain.png",
+        image: "https://github.com/user-attachments/assets/319741e5-81c9-499b-a1c7-5a5f2888d398",
         category: "personal-project",
         description: "Story based app where user can read , and upload their Stories.Developed in 1 month.Users must register their account to access this app.",
         link: "https://github.com/KrishnanditoLksn/CeritainCerita"
