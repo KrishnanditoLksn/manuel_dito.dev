42b490b302297ba1108386f38e31a56bedca0ad0
Gorthdzx <krislaksana123@gmail.com>
2024-12-26 19:26:25 +0700

    refactor: import DescriptionSection

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,15 +1,15 @@
 import Header from "./navbar/Navbar.jsx";
 import About from "../About.jsx";
-import ContentSection from "./section/ContentSection.jsx";
 import ProjectSection from "./project/ProjectSection.jsx";
 import {FooterSection} from "./footer/FooterSection.jsx";
+import DescriptionSection from "./section/DescriptionSection.jsx";
 
 export default function Pages() {
     return (
         <main className="text-gray-400 bg-white body-font">
             <Header></Header>
             <About></About>
-            <ContentSection></ContentSection>
+            <DescriptionSection></DescriptionSection>
             <ProjectSection></ProjectSection>
             <FooterSection></FooterSection>
         </main>
