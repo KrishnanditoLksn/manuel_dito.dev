c45cdfe4e7c69444a5800c6c8d45d8f2d693e4cf
Gorthdzx <krislaksana123@gmail.com>
2024-12-26 12:11:15 +0700

    feat: call Pages file  that consists all of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import About from "./components/About.jsx";
-import Header from "./components/pages/navbar/Navbar.jsx";
-import ContentSection from "./components/pages/section/ContentSection.jsx";
-import ProjectSection from "./components/pages/project/ProjectSection.jsx";
-import {FooterSection} from "./components/pages/footer/FooterSection.jsx";
+import Pages from "./components/pages/Pages.jsx";
 
 function App() {
     return (
-        <main className="text-gray-400 bg-white body-font">
-            <Header></Header>
-            <About></About>
-            <ContentSection></ContentSection>
-            <ProjectSection></ProjectSection>
-            <FooterSection></FooterSection>
-        </main>
+        <Pages></Pages>
     );
 }
 
