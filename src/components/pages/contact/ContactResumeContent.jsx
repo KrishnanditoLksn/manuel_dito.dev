2896b2571bc3af96081fe962baebe2d1c7ef92d3
Gorthdzx <krislaksana123@gmail.com>
2024-12-27 18:14:53 +0700

    feat : added resume  content and description

---
+++ src/components/pages/contact/ContactResumeContent.jsx
@@ -0,0 +1,7 @@
+import ContactResumeDescription from "./ContactResumeDescription.jsx";
+
+export default function ContactResumeContent() {
+    return (
+        <ContactResumeDescription></ContactResumeDescription>
+    )
+}
---
+++ src/components/pages/contact/ContactResumeDescription.jsx
@@ -0,0 +1,11 @@
+export default function ContactResumeDescription() {
+    return (
+        <div id="/resume" className="ml-[400px] mt-80 flex flex-col">
+            <div className="items-center mt-60 mb-56">
+                <h1>
+                    Hey there I Have a Resume
+                </h1>
+            </div>
+        </div>
+    )
+}
