8033e91ef5e8efe6dff4154a90137ec4578c7820
Gorthdzx <krislaksana123@gmail.com>
2024-12-27 22:25:50 +0700

    refactor : changed height to 15

--- src/components/pages/navbar/Navlist.jsx
+++ src/components/pages/navbar/Navlist.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-scroll";
 const Navlist = () => {
     return (
         <div className="max-w-5xl mx-auto px-4">
-            <div className="flex items-center justify-between h-12">
+            <div className="flex items-center justify-between h-15">
                 <div className=" ml-96 flex space-x-4 text-gray-900">
                     <a href="/about">
                         <Link to="about"
