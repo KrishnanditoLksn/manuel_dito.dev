25348b794c48a96dbc0f37899997d42fbef3f0ac
Gorthdzx <krislaksana123@gmail.com>
2024-12-28 22:22:45 +0700

    feat : added responsive layout for about section

--- src/components/pages/about/AboutSection.jsx
+++ src/components/pages/about/AboutSection.jsx
@@ -1,30 +1,23 @@
-import {swipeDown} from "../../../helper/ScrollDown.js";
-import {SlArrowDown} from "react-icons/sl";
-
 const AboutDescription = () => {
     return (
-        <div id="/about" className=" ml-40 mx-auto flex px-14 py-20 md:flex-row flex-col items-center bg-white">
-            <div
-                className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black  ">
-                    Hi,Emmanuel Dito is here
-                </h1>
-                <br className="hidden lg:inline-block"/>
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-black  ">
-                    Mobile Native Enthusiast;
-                </h1>
-                <p className="mb-2  text-2xl leading-relaxed text-black">
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    Hello everyone,I'm Dito from Universitas Sanata Dharma.Nice to meet you guys.
-                    I always trust that the best way to learn is by doing, so I always try to make a project to improve
-                    my skills.
-                    So far counted from 2024, I have been a Yoasobi fans and I am very interested in Mobile Development.
-                </p>
-                <br className="hidden lg:inline-block"></br>
-                <button className="mt-80 ml-[520px] mr-80 text-center text-black font-bold text-2xl">
-                    <SlArrowDown size="40px"
-                                 onClick={() => swipeDown(1000)}/>
-                </button>
+        <div id="about"
+             className="bg-white dark:bg-white gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mt-52 mb-52">
+            <div>
+                <div className="font-light text-black text-2xl sm:text-lg dark:text-gray-400 mb-8">
+                    <h2 className="mb-4 text-3xl sm:text-4xl tracking-tight font-extrabold text-black dark:text-black">
+                        Hi,Emmanuel Dito is here
+                    </h2>
+                    <p className="mb-4 dark:text-black  title-font sm:text-4xl text-2xl font-semibold text-black ">
+                        Mobile Enthusiast;
+                    </p>
+                    <p className="mb-2  text-xl leading-relaxed  text-black">
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        Hello everyone,I'm Dito from Universitas Sanata Dharma.Nice to meet you guys.I always
+                        trust that the best way to learn is by doing, so I always try to make a project to improve
+                        my skills. So far counted from 2024, I have been a Yoasobi fans and I am very interested in
+                        Mobile Development.
+                    </p>
+                </div>
             </div>
         </div>
     )
