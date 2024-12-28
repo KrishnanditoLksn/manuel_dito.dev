d357d892951a85d5d18905dc0f983020b86ec484
Gorthdzx <krislaksana123@gmail.com>
2024-12-28 21:23:36 +0700

    delete : removed nav background

--- src/components/pages/navbar/Navbar.jsx
+++ src/components/pages/navbar/Navbar.jsx
@@ -2,9 +2,7 @@ import Navlist from "./Navlist"
 
 const Navbar = () => {
     return (
-        <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-50">
-            <Navlist></Navlist>
-        </nav>
+        <Navlist></Navlist>
     )
 }
 
