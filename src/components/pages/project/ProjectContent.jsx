1584d91b517923ce866119f1350a3804ef6a8c26
Gorthdzx <krislaksana123@gmail.com>
2024-12-28 21:01:08 +0700

    feat : dude what the space

--- src/components/pages/project/ProjectContent.jsx
+++ src/components/pages/project/ProjectContent.jsx
@@ -4,7 +4,7 @@ export default function ProjectContent() {
     return (
         <section className="bg-white">
             <div id="/projects"
-                className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-42">
+                 className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 mr-42">
                 <div>
                     <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">
